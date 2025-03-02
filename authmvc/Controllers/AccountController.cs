9a8150b525f49cddeef8f0b4c988cdb03680e05d
jcbcouch <jcbcouch@gmail.com>
2025-03-02 14:09:18 -0600

    register view

---
+++ authmvc/Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using authmvc.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace authmvc.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            RegisterViewModel registerViewModel = new();
+            return View(registerViewModel);
+        }
+    }
+}
