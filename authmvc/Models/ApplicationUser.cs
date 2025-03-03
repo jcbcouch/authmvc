b3e83acd2f208a9730947c9a49ad1c84ee01b570
jcbcouch <jcbcouch@gmail.com>
2025-03-03 15:31:30 -0600

    user controller

--- authmvc/Models/ApplicationUser.cs
+++ authmvc/Models/ApplicationUser.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace authmvc.Models
 {
@@ -7,5 +8,9 @@ namespace authmvc.Models
     {
         [Required]
         public string Name { get; set; }
+        [NotMapped]
+        public string RoleId { get; set; }
+        [NotMapped]
+        public string Role { get; set; }
     }
 }
